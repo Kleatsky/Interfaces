e2ab1f33d79deb78f5332d86c6159049fedb0bda
salaga999999999 <salaga999999999@gmail.com>
2025-03-16 17:36:31 +0700

    Cleared and tested code

--- InterfacesConsole/Program.cs
+++ InterfacesConsole/Program.cs
@@ -4,9 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Quadcopter  quadcopter = new Quadcopter();
+            Quadcopter quadcopter = new Quadcopter();
             Console.WriteLine(quadcopter.GetInfo());
             quadcopter.Charge();
+            //Console.WriteLine((quadcopter as IFlyingRobot)?.GetRobotType());
             Console.WriteLine(quadcopter.GetRobotType());
             Console.WriteLine(String.Join(", ", quadcopter.GetComponents()));
         }
